4e7a41d6fbebc1f38a248d3cff651be1f95a3ac0
dafahfabiansyah <davarobiansyah@gmail.com>
2025-03-21 20:27:37 +0700

    add new func : ReadFile

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"fmt"
+
 	"github.com/dafahfabiansyah/go-file-parser/parser"
 )
 
 func main() {
-	parser.CreateNewFile("file.txt", "hello world")
+	// create file
+	fmt.Println("file has created", parser.CreateNewFile("file.txt", "hello world"))
+	// read file
+	fmt.Println(parser.ReadFile("file.txt"))
+	// update file
+
+	// delete file
 }
--- parser/parser.go
+++ parser/parser.go
@@ -1,6 +1,8 @@
 package parser
 
-import "os"
+import (
+	"os"
+)
 
 func CreateNewFile(name string, message string) error {
 	file, err := os.OpenFile(name, os.O_CREATE|os.O_WRONLY, 0666)
@@ -13,4 +15,17 @@ func CreateNewFile(name string, message string) error {
 
 	_, err = file.WriteString(message)
 	return err
+}
+
+func ReadFile(name string) (string, error) {
+	file, err := os.Open(name)
+
+	if err != nil {
+		return "", err
+	}
+
+	defer file.Close()
+
+	content, err := os.ReadFile(name)
+	return string(content), err
 }
